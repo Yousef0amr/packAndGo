dab937ef04cabfbf0cf8bb09a5e316bd238d3b93
Youssief Amr Mohamed <youssief192677@fci.bu.edu.eg>
2024-03-09 13:32:43 +0200

    up

--- src/modules/property/controllers/getAllProperties.js
+++ src/modules/property/controllers/getAllProperties.js
@@ -2,7 +2,7 @@ const wrap = require('express-async-wrapper')
 const Property = require('../property.model')
 const { Success } = require('../../../utils/apiResponse')
 const getAll = require('../../../common/DB_operation/CRUD/getAll')
-const ApiFeatures = require('./../../../utils/APiFeatures')
+const ApiFeatures = require('./../../../utils/apiFeatures')
 const getAllProperty = wrap(
     async (req, res, next) => {
 
